d4e9f77f2b9f5256a64c91f84baab7ca19686733
Wolfgang Fahl <wf@bitplan.com>
2021-10-22 10:01:04 +0200

    starts persistence for Archive

--- scan/dms.py
+++ scan/dms.py
@@ -7,8 +7,47 @@ see http://diagrams.bitplan.com/render/png/0xe1f1d160.png
 see http://diagrams.bitplan.com/render/txt/0xe1f1d160.txt
 
 '''
+from lodstorage.jsonable import JSONAble
+from lodstorage.entity import EntityManager
+from lodstorage.storageconfig import StorageConfig
 
-class Archive(object):
+class DMSStorage:
+    '''
+    Document management system storage configuration
+    '''
+    profile=True
+    withShowProgress=True
+    
+    @staticmethod
+    def getStorageConfig(debug:bool=False,mode='sql')->StorageConfig:
+        '''
+        get the storageConfiguration
+        
+        Args:
+            debug(bool): if True show debug information
+            mode(str): sql or json
+        
+        Return:
+            StorageConfig: the storage configuration to be used
+        '''
+        if mode=='sql':
+            config=StorageConfig.getSQL(debug=debug)
+        elif mode=='json':
+            config=StorageConfig.getJSON()
+        elif mode=='jsonpickle':
+            config=StorageConfig.getJsonPickle(debug=debug)
+        else:
+            raise Exception(f"invalid mode {mode}")
+        config.cacheDirName="dms"
+        cachedir=config.getCachePath() 
+        config.profile=DMSStorage.profile
+        config.withShowProgress=DMSStorage.withShowProgress
+        if mode=='sql':
+            config.cacheFile=f"{cachedir}/dms.db"
+        return config
+    
+
+class Archive(JSONAble):
     '''
     an Archive might be a filesystem 
     on a server or a (semantic) mediawiki
@@ -29,3 +68,25 @@ class Archive(object):
         }]
         return samplesLOD
         
+
+class ArchiveManager(EntityManager):
+    '''
+    manager for Archives
+    '''
+    
+    def __init__(self,mode='sql',debug=False):
+        '''constructor
+        '''
+        name="archive"
+        entityName="Archive"
+        entityPluralName="archives"
+        listName=entityPluralName
+        clazz=Archive
+        tableName=name
+        config=DMSStorage.getStorageConfig(mode=mode,debug=debug)
+        handleInvalidListTypes=True
+        filterInvalidListTypes=True
+        primaryKey='url'
+        super().__init__(name, entityName, entityPluralName, listName, clazz, tableName, primaryKey, config, handleInvalidListTypes, filterInvalidListTypes, debug)
+   
+
--- tests/testdms.py
+++ tests/testdms.py
@@ -5,7 +5,7 @@ Created on 22.10.2021
 '''
 import unittest
 from tests.basetest import BaseTest
-from scan.dms import Archive
+from scan.dms import Archive,ArchiveManager
 
 class TestDMS(BaseTest):
     '''
@@ -13,6 +13,13 @@ class TestDMS(BaseTest):
     '''
 
     def testArchive(self):
+        am=ArchiveManager(mode='json')
+        for archiveRecord in Archive.getSamples():
+            archive=Archive()
+            archive.fromDict(archiveRecord)
+            am.getList().append(archive)
+        jsonStr=am.toJSON(limitToSampleFields=True)
+        print(jsonStr)
         pass
 
 
