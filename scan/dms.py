73c8fa28ab2f4bc20880d183626b7fa8f8dca246
Wolfgang Fahl <wf@bitplan.com>
2021-10-22 13:50:18 +0200

    adds folder scanning

--- scan/dms.py
+++ scan/dms.py
@@ -10,6 +10,9 @@ see http://diagrams.bitplan.com/render/txt/0xe1f1d160.txt
 from lodstorage.jsonable import JSONAble
 from lodstorage.entity import EntityManager
 from lodstorage.storageconfig import StorageConfig
+import re
+import os
+import sys
 
 class DMSStorage:
     '''
@@ -46,7 +49,52 @@ class DMSStorage:
             config.cacheFile=f"{cachedir}/dms.db"
         return config
     
+class Folder(JSONAble):
+    '''
+    a Folder might be a filesystem folder or a category in a wiki
+    '''
 
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+    @classmethod
+    def getSamples(cls):
+        samplesLOD = [{
+            "name": "wiki.bitplan.com",
+            "path": "media",
+            "url": "",
+            "archiveUrl": "http://wiki.bitplan.com",
+        }]
+        return samplesLOD
+    
+class FolderManager(EntityManager):
+    '''
+    manager for Archives
+    '''
+    
+    def __init__(self,mode='sql',debug=False):
+        '''constructor
+        '''
+        name="folder"
+        entityName="Folder"
+        entityPluralName="folders"
+        listName=entityPluralName
+        clazz=Folder
+        tableName=name
+        config=DMSStorage.getStorageConfig(mode=mode,debug=debug)
+        handleInvalidListTypes=True
+        filterInvalidListTypes=True
+        primaryKey='url'
+        super().__init__(name, entityName, entityPluralName, listName, clazz, tableName, primaryKey, config, handleInvalidListTypes, filterInvalidListTypes, debug)
+   
+    @staticmethod
+    def getInstance(mode='sql'):
+        fm=FolderManager(mode=mode)
+        fm.fromCache()
+        return fm
+    
 class Archive(JSONAble):
     '''
     an Archive might be a filesystem 
@@ -61,14 +109,44 @@ class Archive(JSONAble):
     @classmethod
     def getSamples(cls):
         samplesLOD = [{
+            "server": "wiki.bitplan.com",
+            "name": "wiki",
+            "url": "http://wiki.bitplan.com",
+            "wikiid": "wiki",
+        },{
             "server": "media.bitplan.com",
             "name": "media",
             "url": "http://media.bitplan.com",
             "wikiid": "media",
         }]
         return samplesLOD
+    
+    def getFolders(self)->list:
+        '''
+        get the folders of this archive
+        '''
+        folderList=[]
+        # this archive is pointing to a wiki
+        if hasattr(self,"wikiid") and self.wikiid is not None:
+            askQuery=""
+        else:
+            # this archive is pointing to folder
+            pattern=fr"http://{self.server}/"
+            folderPath=re.sub(pattern,"",self.url)
+            if sys.platform == "darwin":
+                folderPath=f"/Volumes/{folderPath}"
+            for _root, dirs, _files in os.walk(folderPath):
+                for dirname in dirs: 
+                    if not dirname.startswith("."):
+                        folder=Folder()
+                        folder.archiveUrl=self.url
+                        folder.path=os.path.join(_root, dirname)
+                        folder.name=dirname
+                        folderList.append(folder)  
+            pass
+        return folderList
+    
         
-
 class ArchiveManager(EntityManager):
     '''
     manager for Archives
@@ -90,7 +168,11 @@ class ArchiveManager(EntityManager):
         super().__init__(name, entityName, entityPluralName, listName, clazz, tableName, primaryKey, config, handleInvalidListTypes, filterInvalidListTypes, debug)
    
     @staticmethod
-    def getInstance():
-        am=ArchiveManager(mode='json')
+    def getInstance(mode='json'):
+        am=ArchiveManager(mode=mode)
         am.fromCache()
         return am
+    
+    
+
+
--- tests/testdms.py
+++ tests/testdms.py
@@ -5,43 +5,81 @@ Created on 22.10.2021
 '''
 import unittest
 from tests.basetest import BaseTest
-from scan.dms import Archive,ArchiveManager
+from scan.dms import Archive,ArchiveManager, Folder, FolderManager
 
 class TestDMS(BaseTest):
     '''
     test document management system handling
     '''
-
-    def testArchive(self):
+    
+    def setUp(self):
         '''
-        test the Archive concept
+        setup this test
+        '''
+        BaseTest.setUp(self)
+        if self.isInPublicCI():
+            am=self.getSampleArchiveManager()
+            am.store()
+        
+    def getSampleArchiveManager(self):
+        '''
+        get an archive manager based on the samples
         '''
         am=ArchiveManager(mode='json')
         for archiveRecord in Archive.getSamples():
             archive=Archive()
             archive.fromDict(archiveRecord)
             am.getList().append(archive)
+        return am
+
+    def testArchive(self):
+        '''
+        test the Archive concept
+        '''
+        am=self.getSampleArchiveManager()
         jsonStr=am.toJSON(limitToSampleFields=True)
         expected='''{
-    "archives": [
-        {
             "name": "media",
             "server": "media.bitplan.com",
             "url": "http://media.bitplan.com",
             "wikiid": "media"
-        }
-    ]
-}'''
+        }'''
         if self.debug:
             print(jsonStr)
-        self.assertEqual(expected,jsonStr)    
-        if self.isInPublicCI():
-            am.store()
+        self.assertTrue(expected in jsonStr)    
         am2=ArchiveManager.getInstance()
         archives=am2.archives
         self.assertTrue(len(archives)>0)
         pass
-
+    
+    def testFolders(self):
+        '''
+        test folder handling
+        '''
+        am=self.getSampleArchiveManager()
+        archivesByUrl,_dup=am.getLookup("url")
+        archives=[archivesByUrl["http://wiki.bitplan.com"]]
+        expected={}
+        expected["wiki"]=0
+        fm=FolderManager(mode="json")
+        if not self.inPublicCI():
+            archive=Archive()
+            archive.name="bitplan-scan"
+            archive.server="capri.bitplan.com"
+            archive.url="http://capri.bitplan.com/bitplan/scan/"
+            archives.append(archive)
+            expected["bitplan-scan"]=10
+        for archive in archives:
+            folders=archive.getFolders()
+            folderCount=len(folders)
+            msg=f"found {folderCount} folders in {archive.name}"
+            self.debug=True
+            if self.debug:
+                print(msg)
+            self.assertTrue(folderCount>=expected[archive.name])
+        if not self.inPublicCI():
+            fm.folders=folders
+            fm.store()
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
