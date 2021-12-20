83f7245cb91e9bad0d9f9b1373439885d465cc67
Wolfgang Fahl <wf@bitplan.com>
2021-12-20 10:12:26 +0100

    prepares 0.0.11 release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='scan2wiki',
-    version='0.0.10',
+    version='0.0.11',
 
     packages=['scan'],
     classifiers=[
