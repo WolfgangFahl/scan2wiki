561db12521bee8c18d0184634a0c1ab0a00b175c
Wolfgang Fahl <wf@bitplan.com>
2022-07-22 09:30:02 +0200

    preparese new release

--- scan/version.py
+++ scan/version.py
@@ -9,9 +9,9 @@ class Version(object):
     Version handling for scan2wiki
     '''
     name="scan2wiki"
-    version='0.0.13'
+    version='0.0.14'
     description='Scan to Wiki by watching a scan folder'
     date = '2021-12-20'
-    updated = '2022-04-08'
+    updated = '2022-07-22'
 
         
