db781d042b037fbd92c7f48a07061c17eb648ee1
Wolfgang Fahl <wf@bitplan.com>
2023-11-14 12:55:49 +0100

    refactors

--- scan/scan_webserver.py
+++ scan/scan_webserver.py
@@ -4,31 +4,60 @@ Created on 2023-11-14
 @author: wf
 """
 import os
+from nicegui import ui, Client
+from ngwidgets.lod_grid import ListOfDictsGrid
 from ngwidgets.input_webserver import InputWebserver
 from ngwidgets.webserver import WebserverConfig
 from scan.version import Version
+from scan.dms import DMSStorage
+from scan.scans import Scans
+
 
 class ScanWebServer(InputWebserver):
     """
     server for Document Management system with option to scan to Semantic Mediawikis
     """
+
     @classmethod
-    def get_config(cls)->WebserverConfig:
+    def get_config(cls) -> WebserverConfig:
         """
         get the configuration for this Webserver
         """
-        copy_right="(c)2020-2023 Wolfgang Fahl"
-        config=WebserverConfig(copy_right=copy_right,version=Version(),default_port=8334)
+        copy_right = "(c)2020-2023 Wolfgang Fahl"
+        config = WebserverConfig(
+            copy_right=copy_right, version=Version(), default_port=8334
+        )
         return config
-    
+
     def __init__(self):
         """Constructs all the necessary attributes for the WebServer object."""
-        InputWebserver.__init__(self,config=ScanWebServer.get_config())
-        
+        InputWebserver.__init__(self, config=ScanWebServer.get_config())
+        scandir = DMSStorage.getScanDir()
+        self.scans = Scans(scandir)
+
     @classmethod
-    def examples_path(cls)->str:
+    def examples_path(cls) -> str:
         # the root directory (default: examples)
-        path = os.path.join(os.path.dirname(__file__), '../scan2wiki_examples')
+        path = os.path.join(os.path.dirname(__file__), "../scan2wiki_examples")
         path = os.path.abspath(path)
         return path
-  
+    
+    async def update_scans(self):
+        """
+        update the scans grid
+        """
+        try:
+            lod=self.scans.get_scan_files()
+            self.lod_grid.load_lod(lod)
+        except Exception as ex:
+            self.handle_exception(ex,self.do_trace)
+    
+    async def home(self, _client: Client):
+        """
+        provide the main content page
+
+        """
+        self.setup_menu()
+        self.lod_grid = ListOfDictsGrid(auto_size_columns=True)
+        await self.update_scans()
+        await self.setup_footer()
---
+++ scan/scans.py
@@ -0,0 +1,69 @@
+"""
+Created on 2023-11-14
+
+@author: wf
+"""
+
+import os
+from datetime import datetime
+from ngwidgets.widgets import Link
+from typing import List, Dict
+
+
+class Scans:
+    """
+    Class to handle operations related to scanned files.
+    """
+
+    def __init__(self, scandir: str):
+        """
+        Initialize the Scans object.
+
+        Args:
+            scandir (str): The directory where the scanned files are located.
+        """
+        self.scandir = scandir
+
+    def get_full_path(self, path: str) -> str:
+        """
+        Generate the full path for a given relative path.
+
+        Args:
+            path (str): The relative path to be resolved.
+
+        Returns:
+            str: The full path combining the scandir and the provided relative path.
+        """
+        fullpath = os.path.join(self.scandir, path)
+        return fullpath
+
+    def get_scan_files(self) -> List[Dict[str, object]]:
+        """
+        Retrieve the scanned files information from the directory.
+
+        Returns:
+            List[Dict[str, object]]: A list of dictionaries, each representing a file.
+            Each dictionary contains details like file name, last modified time, size, and links for delete and upload actions.
+        """
+        scan_files = []
+        for index,path in enumerate(os.listdir(self.scandir)):
+            try:
+                fullpath = self.get_full_path(path)
+                ftime = datetime.fromtimestamp(os.path.getmtime(fullpath))
+                ftimestr = ftime.strftime("%Y-%m-%d %H:%M:%S")
+                size = os.path.getsize(fullpath)
+                file_link = Link.create(url=f"/files/{path}", text=path)
+                scan_file = {
+                    "#": index+1,
+                    "name": file_link,
+                    "lastModified": ftimestr,
+                    "size": size,
+                    "delete": Link.create(url=f"/delete/{path}", text="❌"),
+                    "upload": Link.create(url=f"/upload/{path}", text="⇧"),
+                }
+                scan_files.append(scan_file)
+            except Exception as ex:
+                msg = f"error {str(ex)} for {path}"
+                raise Exception(msg)
+
+        return scan_files
