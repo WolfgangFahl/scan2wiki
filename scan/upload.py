0edfc7c6c88855d764f66fbc333ee3d461c2eb1a
Wolfgang Fahl <wf@bitplan.com>
2023-11-15 10:30:56 +0100

    adds red link / blue link handling

--- scan/upload.py
+++ scan/upload.py
@@ -7,6 +7,7 @@ from nicegui import ui
 from scan.dms import Document
 from datetime import datetime
 from ngwidgets.widgets import Link
+from nicegui.run import cpu_bound
 
 class UploadForm:
     """
@@ -16,6 +17,8 @@ class UploadForm:
         """
         constructor
         """
+        self.red_link="color: red;text-decoration: underline;"
+        self.blue_link="color: blue;text-decoration: underline;"
         self.webserver=webserver
         self.wiki_users=wiki_users
         self.scandir=self.webserver.scandir
@@ -24,9 +27,9 @@ class UploadForm:
         self.submit = ui.button('upload', on_click=self.submit_form)
         self.ocr=ui.button("ocr",on_click=self.run_ocr)
         self.page_title = ui.input('pagetitle', on_change=self.update).props("size=80").bind_value_to(self.doc,"pageTitle")
-        self.page_link = ui.html('pagelink')
+        self.page_link = ui.html('pagelink').style(self.red_link) 
         wiki_selection=list(sorted(wiki_users.keys()))
-        self.wiki_user_select = webserver.add_select('Wiki', wiki_selection)  
+        self.wiki_user_select = webserver.add_select(title='Wiki',selection=wiki_selection,on_change=self.update)  
         self.scanned_file = ui.input('scannedFile',value=path).props("size=80")    
         current_date = datetime.now()
         self.categories = ui.input('categories',value=str(current_date.year)).bind_value_to(self.doc,"categories")
@@ -34,6 +37,9 @@ class UploadForm:
         self.ocr_text_area = ui.textarea('Text').props('clearable').props("rows=15;cols=60").bind_value_to(self.doc,"ocrText")
   
     async def run_ocr(self):
+        """
+        run the optical character recognition
+        """
         ui.notify("Optical Character Recognition requested ...")
         ocr_text=self.doc.getOcrText()
         self.ocr_text_area.value=ocr_text
@@ -49,6 +55,7 @@ class UploadForm:
             wiki_url=f"{wiki_user.url}{wiki_user.scriptPath}"
             wiki_link = Link.create(f"{wiki_url}/index.php/{page_title}",page_title)
             self.page_link.content=wiki_link
+            self.page_link.style=self.red_link
 
     def to_document(self, scandir,withOcr:bool=False):
         """
@@ -69,4 +76,5 @@ class UploadForm:
         uploadDoc=self.doc
         wiki_id=self.wiki_user_select.value
         uploadDoc.uploadFile(wiki_id)
+        self.page_link.style=self.blue_link
         pass
